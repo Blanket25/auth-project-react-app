7a7a78ca8e9b43d08095c1376b346337ba96e804
Blanket25 <bianca_inga@hotmail.com>
2021-10-22 15:50:00 +0200

    correct the routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <div className="flex-container">
+        <Home />
+        <button>
+          <Link to="/log-in">Log in</Link>
+        </button>
+        <button>
+          <Link to="/sing-up">Sign up</Link>
+        </button>
+
+        <Route path="/log-in" component={Login} />
+        <Route path="/sign-up" component={SignUp} />
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,8 @@
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import Login from "./Login";
-
 const Home = () => {
   return (
-    <BrowserRouter>
-      <div className="home-container">
-        <p>WELCOME!</p>
-        <Link to="/log-in">
-          <button>Log in</button>
-        </Link>
-
-        <Route path="/log-in" component={Login} />
-      </div>
-    </BrowserRouter>
+    <div>
+      <p>WELCOME!</p>
+    </div>
   );
 };
 
